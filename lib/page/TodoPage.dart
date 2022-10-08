eeba968c406b354fad5af4326c3cb0c1cfcce03b
zoops <jhbaik@gmail.com>
2022-10-08 13:50:45 +0900

    투두 입력

--- lib/page/TodoPage.dart
+++ lib/page/TodoPage.dart
@@ -17,45 +17,62 @@ class TodoModel
 
 class _TodoPageState extends State<TodoPage> {
 
-  final List<TodoModel> _todoList = [
-    TodoModel(title: 'title1', content: 'content1'),
-    TodoModel(title: 'title2', content: 'content2'),
-    TodoModel(title: 'title3', content: 'content3'),
-    TodoModel(title: 'title1', content: 'content1'),
-    TodoModel(title: 'title2', content: 'content2'),
-    TodoModel(title: 'title3', content: 'content3'),
-    TodoModel(title: 'title1', content: 'content1'),
-    TodoModel(title: 'title2', content: 'content2'),
-    TodoModel(title: 'title3', content: 'content3'),
-    TodoModel(title: 'title1', content: 'content1'),
-    TodoModel(title: 'title2', content: 'content2'),
-    TodoModel(title: 'title3', content: 'content3'),
-  ];
+  final List<TodoModel> _todoList = [];
+  final _todoController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    body: ListView.builder(
-      itemCount: _todoList.length,
-      itemBuilder: (context, index) {
-        return listItemWidget(_todoList[index]);
-      },
-      ),
+    body: Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+            itemCount: _todoList.length,
+            itemBuilder: (context, index) {
+              return listItemWidget(_todoList[index]);
+            },
+            ),
+        ),
+        Row(
+           children: [
+            Expanded(
+              child: TextField(
+                controller: _todoController ,
+                decoration: const InputDecoration(
+                  hintText: 'Title',
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print(_todoController.text);
+                setState(() {
+                  _todoList.add(TodoModel(
+                      title: _todoController.text,
+                      content: _todoController.text, ));
+                });
+                _todoController.clear();
+
+              },
+              child: const Text('Add'),
+            ),
+          ],
+        ),
+      ],
+    ),
     );
   }
 
   Widget listItemWidget(TodoModel todo) {
-    return Expanded(
-      child: Container(
-        margin: const EdgeInsets.all(10),
-        color: Colors.cyanAccent,
-          height: 100,
-          child: Column(
-            children: [
-              Text(todo.title),
-              Text(todo.content),
-            ],
-          )),
-    );
+    return Container(
+      margin: const EdgeInsets.all(10),
+      color: Colors.cyanAccent,
+        height: 100,
+        child: Column(
+          children: [
+            Text(todo.title),
+            Text(todo.content),
+          ],
+        ));
   }
 }
